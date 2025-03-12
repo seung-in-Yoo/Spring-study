a88a645e5db0753cce95700da588775444e62b58
seungin <tmddls91@inha.edu>
2025-03-13 02:53:14 +0900

    비즈니스 요구사항 정리 ~ 회원 서비스 테스트

---
+++ src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+
+// 4가지 기능 만듬
+public interface MemberRepository {
+    Member save(Member member); // 멤버 저장
+    Optional<Member> findById(Long id); // id와 name으로 찾아올수 있음
+    Optional<Member> findMyName(String name);
+    List<Member> findAll(); // 지금까지 저장된 모든 회원 리스트 반환
+}
